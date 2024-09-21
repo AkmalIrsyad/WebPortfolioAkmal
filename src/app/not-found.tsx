c0ce5e947e8cfb26bfe56761e379f5f8ede0620b
Akmal Irsyad <akmalirsyad137@gmail.com>
2024-09-21 23:39:56 +0700

    Update project with new changes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,14 +11,14 @@ function notFound() {
 
     <p className="text-2xl font-bold tracking-tight sm:text-4xl">nyari apa sih? Nih nggak ketemu. 404!</p>
     <Marquee items={items}/>
-  </div>
-  
-  <a
+
+    <a
       href="/"
-      className="border-border dark:border-darkBorder dark:bg-main dark:text-darkText shadow-light dark:shadow-dark cursor-pointer rounded-base border-2 bg-main px-4 py-2 text-center text-sm font-base transition-all hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none sm:text-base dark:hover:shadow-none"
+      className="inline-block border-border dark:border-darkBorder dark:bg-main dark:text-darkText shadow-light dark:shadow-dark cursor-pointer rounded-base border-2 bg-main px-5 py-3 text-center text-sm font-base transition-all hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none sm:text-base dark:hover:shadow-none"
     >
       Go Back Home
     </a>
+  </div>
 </div>
     </div>
   )
