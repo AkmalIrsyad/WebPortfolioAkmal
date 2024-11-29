10171cfa6cb5593c67552d26b6eefb3b6bc32716
Akmal Irsyad <akmalirsyad137@gmail.com>
2024-11-29 23:22:52 +0700

    nambahin project gogoroOto

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,37 +5,46 @@ const PROJECTS: {
   repoUrl: string
   liveLink: string
 }[] = [
-  {
-    name: 'WebBarudak 2KA28 ',
-    description: `Website Kelas Kampus menggunakan Framework Laravel + tailwindWebsite Kelas menggunakan Framework Laravel + tailwind,
+    {
+      name: 'GogoroOto ',
+      description: `GogoroOto adalah platform web yang mempermudah jual beli mobil baik baru maupun bekas. Website tersebut menampilkan katalog mobil dengan informasi lengkap seperti spesifikasi, harga, dan gambar mobil yang menarik.
+      Dengan TechStack Nextjs (TypeScript) dan Tailwind CSS`,
+      liveLink: 'https://gogoro-oto.vercel.app/about',
+      previewImage: '/gogoroOto.png',
+      repoUrl: 'https://github.com/AkmalIrsyad/GogoroOto',
+    },
+
+    {
+      name: 'WebBarudak 2KA28 ',
+      description: `Website Kelas Kampus menggunakan Framework Laravel + tailwindWebsite Kelas menggunakan Framework Laravel + tailwind,
 *Menggunakan CRUD sederhana dan relasi One to Many dengan database MySQL.
 **Website ini menggunakan API OpenWeatherMap untuk mengambil data cuaca saat ini. Integrasi ini memungkinkan pengguna untuk mendapatkan informasi cuaca real-time, termasuk suhu, kelembapan, kecepatan angin, dan kondisi cuaca lainnya untuk lokasi yang dipilih.`,
-    liveLink: 'https://web2ka28.my.id/dashboard',
-    previewImage: '/webbarudak.png',
-    repoUrl: 'https://github.com/AkmalIrsyad/web2ka28',
-  },
-  {
-    name: 'fetching Weather API data with golang web',
-    description: `Memahami Integrasi API dengan Golang: Memperdalam pemahaman tentang bagaimana mengintegrasikan API dengan aplikasi yang dikembangkan menggunakan Golang.
+      liveLink: 'https://web2ka28.my.id/dashboard',
+      previewImage: '/webbarudak.png',
+      repoUrl: 'https://github.com/AkmalIrsyad/web2ka28',
+    },
+    {
+      name: 'fetching Weather API data with golang web',
+      description: `Memahami Integrasi API dengan Golang: Memperdalam pemahaman tentang bagaimana mengintegrasikan API dengan aplikasi yang dikembangkan menggunakan Golang.
 Mengolah dan Menyajikan Data Cuaca: Mengolah data cuaca yang diambil dari API sehingga dapat ditampilkan dalam format yang informatif dan mudah dipahami oleh pengguna.`,
-    liveLink: 'https://golangweb.my.canva.site/',
-    previewImage: '/go.png',
-    repoUrl: 'https://github.com/AkmalIrsyad/fetching-Weather-API-data-with-golang-web',
-  },
-  {
-    name: 'Freelance Web Design',
-    description: 'I will create website design and landing page in figma',
-    liveLink: 'https://www.fiverr.com/gogoro29/create-design-ui-ux-for-a-website-in-figma',
-    previewImage: '/fvr.png',
-    repoUrl: 'https://github.com/AkmalIrsyad/web2ka28',
-  },
-  {
-    name: 'Aplikasi Management Parkir dengan Vb.net',
-    description: 'Tujuan pembuatan Aplikasi ini untuk menyelesaikan Tugas Akhir Matkul PBO semester 2',
-    liveLink: 'https://www.canva.com/design/DAFnkilDsj0/3vbnbDLYZQvul3hw4xYOcw/view',
-    previewImage: '/dekstop.png',
-    repoUrl: 'https://drive.google.com/file/d/17BESqHqEPP5D1CXw35PmrwWJgjKzcRXu/view?usp=sharing',
-  },
-]
+      liveLink: 'https://golangweb.my.canva.site/',
+      previewImage: '/go.png',
+      repoUrl: 'https://github.com/AkmalIrsyad/fetching-Weather-API-data-with-golang-web',
+    },
+    {
+      name: 'Freelance Web Design',
+      description: 'I will create website design and landing page in figma',
+      liveLink: 'https://www.fiverr.com/gogoro29/create-design-ui-ux-for-a-website-in-figma',
+      previewImage: '/fvr.png',
+      repoUrl: 'https://github.com/AkmalIrsyad/web2ka28',
+    },
+    {
+      name: 'Aplikasi Management Parkir dengan Vb.net',
+      description: 'Tujuan pembuatan Aplikasi ini untuk menyelesaikan Tugas Akhir Matkul PBO semester 2',
+      liveLink: 'https://www.canva.com/design/DAFnkilDsj0/3vbnbDLYZQvul3hw4xYOcw/view',
+      previewImage: '/dekstop.png',
+      repoUrl: 'https://drive.google.com/file/d/17BESqHqEPP5D1CXw35PmrwWJgjKzcRXu/view?usp=sharing',
+    },
+  ]
 
 export default PROJECTS
